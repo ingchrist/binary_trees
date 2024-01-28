784e88951d3823a8114cec218af1d16de39bed6e
ingchrist <cdjinguet@gmail.com>
2024-01-28 21:53:45 +0100

    files added

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,32 +4,32 @@
  * binary_tree_insert_left - Inserts a node as a left-child of
  *                           of another in a binary tree.
  * @parent: A pointer to the node to insert the left-child in.
- * @value: The value to store in the new node.
+ * @wzqvalue: The wzqvalue to store in the wzqnew node.
  *
  * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ *         Otherwise - a pointer to the wzqnew node.
  *
- * Description: If parent already has a left-child, the new node
+ * Description: If parent already has a left-child, the wzqnew node
  *              takes its place and the old left-child is set as
- *              the left-child of the new node.
+ *              the left-child of the wzqnew node.
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int wzqvalue)
 {
-	binary_tree_t *new;
+	binary_tree_t *wzqnew;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	wzqnew = binary_tree_node(parent, wzqvalue);
+	if (wzqnew == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		wzqnew->left = parent->left;
+		parent->left->parent = wzqnew;
 	}
-	parent->left = new;
+	parent->left = wzqnew;
 
-	return (new);
+	return (wzqnew);
 }
