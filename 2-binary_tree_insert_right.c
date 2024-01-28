1f779b54756bcb6e3afad54817d172336c64a725
ingchrist <cdjinguet@gmail.com>
2024-01-28 21:55:19 +0100

    files added

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,32 +4,32 @@
  * binary_tree_insert_right - Insert a node as the right-child
  *                            of another in a binary tree.
  * @parent: A pointer to the node to insert the right-child in.
- * @value: The value to store in the new node.
+ * @wzqvalue: The wzqvalue to store in the wzqnew node.
  *
  * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ *         Otherwise - a pointer to the wzqnew node.
  *
- * Description: If parent already has a right-child, the new node
+ * Description: If parent already has a right-child, the wzqnew node
  *              takes its place and the old right-child is set as
- *              the right-child of the new node.
+ *              the right-child of the wzqnew node.
  */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int wzqvalue)
 {
-	binary_tree_t *new;
+	binary_tree_t *wzqnew;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	wzqnew = binary_tree_node(parent, wzqvalue);
+	if (wzqnew == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
 	{
-		new->right = parent->right;
-		parent->right->parent = new;
+		wzqnew->right = parent->right;
+		parent->right->parent = wzqnew;
 	}
-	parent->right = new;
+	parent->right = wzqnew;
 
-	return (new);
+	return (wzqnew);
 }
