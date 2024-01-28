73c3bbf20ba9e34295a6a7a8fa92a9200a2fa245
ingchrist <cdjinguet@gmail.com>
2024-01-28 22:00:48 +0100

    files added

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
- * @node: A pointer to the node to check.
+ * binary_tree_is_leaf - Checks if a wzqnode is a leaf of a binary tree.
+ * @wzqnode: A pointer to the wzqnode to check.
  *
- * Return: If the node is a leaf - 1.
+ * Return: If the wzqnode is a leaf - 1.
  *         Otherwise - 0.
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *wzqnode)
 {
-	if (node == NULL || node->left != NULL || node->right != NULL)
+	if (wzqnode == NULL || wzqnode->left != NULL || wzqnode->right != NULL)
 		return (0);
 
 	return (1);
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - Checks if a node is a root of a binary tree.
- * @node: A pointer to the node to check.
+ * binary_tree_is_root - Checks if a wzqnode is a root of a binary tree.
+ * @wzqnode: A pointer to the wzqnode to check.
  *
- * Return: If the node is a root - 1.
+ * Return: If the wzqnode is a root - 1.
  *         Otherwise - 0.
  */
-int binary_tree_is_root(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *wzqnode)
 {
-	if (node == NULL || node->parent != NULL)
+	if (wzqnode == NULL || wzqnode->parent != NULL)
 		return (0);
 
 	return (1);
