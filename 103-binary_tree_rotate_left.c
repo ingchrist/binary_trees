0053db22a37b4b178e9036e873de7d80c7070db6
ingchrist <cdjinguet@gmail.com>
2024-01-28 23:05:20 +0100

    files added

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,34 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Left-rotates a binary tree.
- * @tree: A pointer to the root node of the tree to rotate.
+ * binary_tree_rotate_left - Left-rotates a binary wzqtree.
+ * @wzqtree: A pointer to the root node of the wzqtree to rotate.
  *
  * Return: A pointer to the new root node after rotation.
  */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *wzqtree)
 {
-	binary_tree_t *pivot, *tmp;
+	binary_tree_t *wzqpivot, *wzqtmp;
 
-	if (tree == NULL || tree->right == NULL)
+	if (wzqtree == NULL || wzqtree->right == NULL)
 		return (NULL);
 
-	pivot = tree->right;
-	tmp = pivot->left;
-	pivot->left = tree;
-	tree->right = tmp;
-	if (tmp != NULL)
-		tmp->parent = tree;
-	tmp = tree->parent;
-	tree->parent = pivot;
-	pivot->parent = tmp;
-	if (tmp != NULL)
+	wzqpivot = wzqtree->right;
+	wzqtmp = wzqpivot->left;
+	wzqpivot->left = wzqtree;
+	wzqtree->right = wzqtmp;
+	if (wzqtmp != NULL)
+		wzqtmp->parent = wzqtree;
+	wzqtmp = wzqtree->parent;
+	wzqtree->parent = wzqpivot;
+	wzqpivot->parent = wzqtmp;
+	if (wzqtmp != NULL)
 	{
-		if (tmp->left == tree)
-			tmp->left = pivot;
+		if (wzqtmp->left == wzqtree)
+			wzqtmp->left = wzqpivot;
 		else
-			tmp->right = pivot;
+			wzqtmp->right = wzqpivot;
 	}
 
-	return (pivot);
+	return (wzqpivot);
 }
