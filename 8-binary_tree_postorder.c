1bfbdae92ec3ac8011e67fa573064db2e8597580
ingchrist <cdjinguet@gmail.com>
2024-01-28 22:06:05 +0100

    files added

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Traverses a binary tree using post-order traversal.
- * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
+ * binary_tree_postorder - Traverses a binary wzqtree using post-order traversal.
+ * @wzqtree: A pointer to the root node of the wzqtree to traverse.
+ * @wzqfunc: A pointer to a function to call for each node.
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *wzqtree, void (*wzqfunc)(int))
 {
-	if (tree && func)
+	if (wzqtree && wzqfunc)
 	{
-		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);
-		func(tree->n);
+		binary_tree_postorder(wzqtree->left, wzqfunc);
+		binary_tree_postorder(wzqtree->right, wzqfunc);
+		wzqfunc(wzqtree->n);
 	}
 }
