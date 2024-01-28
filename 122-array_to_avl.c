2509c95d2869e33ecff9e0eeb301fb8e806c7d06
ingchrist <cdjinguet@gmail.com>
2024-01-28 23:31:31 +0100

    files added

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,33 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - Builds an AVL tree from an array.
- * @array: A pointer to the first element of the array to be converted.
- * @size: The number of elements in @array.
+ * array_to_avl - Builds an AVL wzqtree from an wzqarray.
+ * @wzqarray: A pointer to the first element of the wzqarray to be converted.
+ * @wzqsize: The number of elements in @wzqarray.
  *
  * Return: A pointer to the root node of the created AVL, or NULL upon failure.
  */
-avl_t *array_to_avl(int *array, size_t size)
+avl_t *array_to_avl(int *wzqarray, size_t wzqsize)
 {
-	avl_t *tree = NULL;
+	avl_t *wzqtree = NULL;
 	size_t i, j;
 
-	if (array == NULL)
+	if (wzqarray == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < wzqsize; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
-			if (array[j] == array[i])
+			if (wzqarray[j] == wzqarray[i])
 				break;
 		}
 		if (j == i)
 		{
-			if (avl_insert(&tree, array[i]) == NULL)
+			if (avl_insert(&wzqtree, wzqarray[i]) == NULL)
 				return (NULL);
 		}
 	}
 
-	return (tree);
+	return (wzqtree);
 }
