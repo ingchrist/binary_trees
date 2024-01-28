8b46d98be33b86bcb0f199291a4ae750b8bd0572
ingchrist <cdjinguet@gmail.com>
2024-01-28 22:38:57 +0100

    files added

--- 100-long_binary_trees_ancestor.c
+++ 100-long_binary_trees_ancestor.c
@@ -1,54 +1,54 @@
 #include "binary_trees.h"
 
-size_t depth(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *wzqtree);
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
+ * @wzqfirst: Pointer to the wzqfirst node.
+ * @wzqsecond: Pointer to the wzqsecond node.
  *
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *wzqfirst,
+		const binary_tree_t *wzqsecond)
 {
-	size_t first_depth, second_depth;
+	size_t wzqfirst_depth, wzqsecond_depth;
 
-	if (first == NULL || second == NULL)
+	if (wzqfirst == NULL || wzqsecond == NULL)
 		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
-	if (first->parent == second->parent)
-		return ((binary_tree_t *)first->parent);
-	if (first == second->parent)
-		return ((binary_tree_t *)first);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
+	if (wzqfirst == wzqsecond)
+		return ((binary_tree_t *)wzqfirst);
+	if (wzqfirst->parent == wzqsecond->parent)
+		return ((binary_tree_t *)wzqfirst->parent);
+	if (wzqfirst == wzqsecond->parent)
+		return ((binary_tree_t *)wzqfirst);
+	if (wzqfirst->parent == wzqsecond)
+		return ((binary_tree_t *)wzqsecond);
 
-	for (first_depth = depth(first); first_depth > 1; first_depth--)
-		first = first->parent;
-	for (second_depth = depth(second); second_depth > 1; second_depth--)
-		second = second->parent;
+	for (wzqfirst_depth = depth(wzqfirst); wzqfirst_depth > 1; wzqfirst_depth--)
+		wzqfirst = wzqfirst->parent;
+	for (wzqsecond_depth = depth(wzqsecond); wzqsecond_depth > 1; wzqsecond_depth--)
+		wzqsecond = wzqsecond->parent;
 
-	if (first == second)
-		return ((binary_tree_t *)first);
-	if (first->parent == second->parent)
-		return ((binary_tree_t *)first->parent);
-	if (first == second->parent)
-		return ((binary_tree_t *)first);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
+	if (wzqfirst == wzqsecond)
+		return ((binary_tree_t *)wzqfirst);
+	if (wzqfirst->parent == wzqsecond->parent)
+		return ((binary_tree_t *)wzqfirst->parent);
+	if (wzqfirst == wzqsecond->parent)
+		return ((binary_tree_t *)wzqfirst);
+	if (wzqfirst->parent == wzqsecond)
+		return ((binary_tree_t *)wzqsecond);
 	else
 		return (NULL);
 }
 
 /**
- * depth - Measures the depth of a node in a binary tree.
- * @tree: A pointer to the node to measure the depth.
+ * depth - Measures the depth of a node in a binary wzqtree.
+ * @wzqtree: A pointer to the node to measure the depth.
  *
- * Return: If tree is NULL, your function must return 0, else return the depth.
+ * Return: If wzqtree is NULL, your function must return 0, else return the depth.
  */
-size_t depth(const binary_tree_t *tree)
+size_t depth(const binary_tree_t *wzqtree)
 {
-	return ((tree->parent != NULL) ? 1 + depth(tree->parent) : 0);
+	return ((wzqtree->parent != NULL) ? 1 + depth(wzqtree->parent) : 0);
 }
