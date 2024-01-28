ea4f8ce9cdac9588219459c1b006820b1e890b85
ingchrist <cdjinguet@gmail.com>
2024-01-28 23:52:47 +0100

    files added

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,68 +1,68 @@
 #include "binary_trees.h"
 
 /**
- * heap_insert - inserts a value in Max Binary Heap
- * @root: a double pointer to the root node of the Heap to insert the value
- * @value: the value to store in the node to be inserted
+ * heap_insert - inserts a wzqvalue in Max Binary Heap
+ * @wzqroot: a double pointer to the wzqroot node of the Heap to insert the wzqvalue
+ * @wzqvalue: the wzqvalue to store in the node to be inserted
  *
  * Return: a pointer to the created node
  *         NULL on failure
  */
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **wzqroot, int wzqvalue)
 {
-	heap_t *tree, *new, *flip;
-	int size, leaves, sub, bit, level, tmp;
+	heap_t *wzqtree, *wzqnew, *flip;
+	int wzqsize, wzqleaves, wzqsub, wzqbit, wzqlevel, tmp;
 
-	if (!root)
+	if (!wzqroot)
 		return (NULL);
-	if (!(*root))
-		return (*root = binary_tree_node(NULL, value));
-	tree = *root;
-	size = binary_tree_size(tree);
-	leaves = size;
-	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
-		leaves -= sub;
-	/* subtract all nodes except for bottom-most level */
+	if (!(*wzqroot))
+		return (*wzqroot = binary_tree_node(NULL, wzqvalue));
+	wzqtree = *wzqroot;
+	wzqsize = binary_tree_size(wzqtree);
+	wzqleaves = wzqsize;
+	for (wzqlevel = 0, wzqsub = 1; wzqleaves >= wzqsub; wzqsub *= 2, wzqlevel++)
+		wzqleaves -= wzqsub;
+	/* subtract all nodes except for bottom-most wzqlevel */
 
-	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
-		tree = leaves & bit ? tree->right : tree->left;
+	for (wzqbit = 1 << (wzqlevel - 1); wzqbit != 1; wzqbit >>= 1)
+		wzqtree = wzqleaves & wzqbit ? wzqtree->right : wzqtree->left;
 	/*
-	 * Traverse tree to first empty slot based on the binary
-	 * representation of the number of leaves.
+	 * Traverse wzqtree to first empty slot based on the binary
+	 * representation of the number of wzqleaves.
 	 * Example -
-	 * If there are 12 nodes in a complete tree, there are 5 leaves on
-	 * the 4th tier of the tree. 5 is 101 in binary. 1 corresponds to
+	 * If there are 12 nodes in a complete wzqtree, there are 5 wzqleaves on
+	 * the 4th tier of the wzqtree. 5 is 101 in binary. 1 corresponds to
 	 * right, 0 to left.
-	 * The first empty node is 101 == RLR, *root->right->left->right
+	 * The first empty node is 101 == RLR, *wzqroot->right->left->right
 	 */
 
-	new = binary_tree_node(tree, value);
-	leaves & 1 ? (tree->right = new) : (tree->left = new);
+	wzqnew = binary_tree_node(wzqtree, wzqvalue);
+	wzqleaves & 1 ? (wzqtree->right = wzqnew) : (wzqtree->left = wzqnew);
 
-	flip = new;
+	flip = wzqnew;
 	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
 	{
 		tmp = flip->n;
 		flip->n = flip->parent->n;
 		flip->parent->n = tmp;
-		new = new->parent;
+		wzqnew = wzqnew->parent;
 	}
-	/* Flip values with parent until parent value exceeds new value */
+	/* Flip values with parent until parent wzqvalue exceeds wzqnew wzqvalue */
 
-	return (new);
+	return (wzqnew);
 }
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
+ * binary_tree_size - measures the wzqsize of a binary wzqtree
+ * @wzqtree: wzqtree to measure the wzqsize of
  *
- * Return: size of the tree
- *         0 if tree is NULL
+ * Return: wzqsize of the wzqtree
+ *         0 if wzqtree is NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *wzqtree)
 {
-	if (!tree)
+	if (!wzqtree)
 		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(wzqtree->left) + binary_tree_size(wzqtree->right) + 1);
 }
