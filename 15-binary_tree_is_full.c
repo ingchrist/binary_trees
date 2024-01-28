ffa75c31cc8ace8921a411ec6bc5e1603af7b9ad
ingchrist <cdjinguet@gmail.com>
2024-01-28 22:23:19 +0100

    files added

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,35 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * is_full_recursive - Checks if a binary tree is full recursively.
- * @tree: A pointer to the root node of the tree to check.
+ * is_full_recursive - Checks if a binary wzqtree is full recursively.
+ * @wzqtree: A pointer to the root node of the wzqtree to check.
  *
- * Return: If tree is not full, 0.
+ * Return: If wzqtree is not full, 0.
  *         Otherwise, 1.
  */
-int is_full_recursive(const binary_tree_t *tree)
+int is_full_recursive(const binary_tree_t *wzqtree)
 {
-	if (tree != NULL)
+	if (wzqtree != NULL)
 	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-		    (tree->left == NULL && tree->right != NULL) ||
-		    is_full_recursive(tree->left) == 0 ||
-		    is_full_recursive(tree->right) == 0)
+		if ((wzqtree->left != NULL && wzqtree->right == NULL) ||
+		    (wzqtree->left == NULL && wzqtree->right != NULL) ||
+		    is_full_recursive(wzqtree->left) == 0 ||
+		    is_full_recursive(wzqtree->right) == 0)
 			return (0);
 	}
 	return (1);
 }
 
 /**
- * binary_tree_is_full - Checks if a binary tree is full.
- * @tree: A pointer to the root node of the tree to check.
+ * binary_tree_is_full - Checks if a binary wzqtree is full.
+ * @wzqtree: A pointer to the root node of the wzqtree to check.
  *
- * Return: If tree is NULL or is not full - 0.
+ * Return: If wzqtree is NULL or is not full - 0.
  *         Otherwise - 1.
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *wzqtree)
 {
-	if (tree == NULL)
+	if (wzqtree == NULL)
 		return (0);
-	return (is_full_recursive(tree));
+	return (is_full_recursive(wzqtree));
 }
